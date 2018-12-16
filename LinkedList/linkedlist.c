85b5054d8fb1276f257b95015dd3543a2bbf3042
Ryan-Red <45923095+Ryan-Red@users.noreply.github.com>
2018-12-16 18:19:08 -0500

    Printing Added and fixed addToTail bug

--- LinkedList/linkedlist.c
+++ LinkedList/linkedlist.c
@@ -12,39 +12,52 @@ int addToHead(llnode **head, int val) {
 	llnode *oldHead;
 	if (head == NULL) { return -1;}
 	oldHead = *head;
+	*head = (llnode *)malloc(sizeof(llnode));
 	(*head)->val = val;
 	(*head)->previous = NULL;
 	(*head)->next = oldHead;
 	((*head)->next)->previous = *head;
+	
 	return 0;
 }
 int addToTail(llnode **head, int val) {
 	llnode *newTail;
-	if (head == NULL) {
-		printf("llnode not initialized");
-		return -1;
-	}
-	if ((*head)->next != NULL) {
-		return addToTail(&((*head)->next), val);
-	}
-	else {
-		newTail = (llnode *)malloc(sizeof(llnode));
-		newTail->val = val;
-		newTail->next = NULL;
-		newTail->previous = *head;
-		(*head)->next = newTail;
+	if (head == NULL) { return -1; }
+	if (*head == NULL) {
+		*head = (llnode *)malloc(sizeof(llnode));
+		(*head)->next = NULL;
+		(*head)->previous = NULL;
+		(*head)->val = val;
 		return 0;
 	}
+	if ((*head)->next == NULL) {
+		((*head)->next) = (llnode *)malloc(sizeof(llnode));
+		((*head)->next)->val = val;
+		((*head)->next)->next = NULL;
+		((*head)->next)->previous = *head;
+		return 0;
+	}
+	else {
+		return addToTail(&((*head)->next), val);
+	}
 
+
+}
+int llprint(llnode *head) {
+	if (head == NULL) { return 0; }
+	else {
+		printf("%d\n", head->val);
+		return (llprint(head->next));
+	}
 }
 int main(void) {
 	llnode* head = NULL;
 
-	head = (llnode*)malloc(sizeof(llnode));
-	addToHead(&head, 15);
-	addToTail(&head, 22);
-	printf("Value at the first node is of: %d", head->val);
-	printf("Tail value is of %d", (head->next)->val);
+	addToTail(&head, 15);
+	addToHead(&head, 22);
+	printf("Value at the first node is of: %d\n", head->val);
+	printf("Tail value is of %d\n", (head->next)->val);
+	llprint(head);
 	return 0;
 
 
